31b9c81136ab15223c6759ba757cba1d519b1cee
sarabperez <152985931+sarabperez@users.noreply.github.com>
2023-12-11 15:09:54 -0300

    archivo js con funciones

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function comparar(a, b, c){
+    if (a>b && a>c) {
+        return (a);
+} else if (b>a && b>c) {
+    return (b);
+} else {
+    return (c);
+}
+ }
+console.log(comparar(300, 931, 58 ));
